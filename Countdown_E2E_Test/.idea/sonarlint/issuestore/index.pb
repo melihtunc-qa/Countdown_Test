e585b66bc3ab86671f89d453c6618e3a330477cb
bronze2727 <melihtunc27@gmail.com>
2024-01-11 16:08:24 +0300

    otomasyon testleri Selenium-Junit5 ile yazıldı

---
+++ Countdown_E2E_Test/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,7 @@
+
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+I
+src/test/java/Benioku.txt,8\d\8df4d66ef5c20e6e2b98c04a5e47b526110ffa3d
+L
+src/test/java/CountDown.java,1\7\175da23c9dcc7871e9da75bb4e50fdc9d9e1e42b
---
+++ Countdown_E2E_Test/src/test/java/CountDown.java
@@ -0,0 +1,130 @@
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class CountDown {
+
+    static EdgeOptions options= new EdgeOptions();
+    static WebDriver driver = new EdgeDriver(options);
+
+    @BeforeClass
+    public static void getDriver() {
+
+        options.addArguments("--disable-notifications");
+        driver.manage().window().maximize();
+        driver.get("http://localhost:3000/");
+    }
+
+    private void zoomPage(WebDriver driver, double zoomFactor) {
+        JavascriptExecutor jsExecutor = (JavascriptExecutor) driver;
+        jsExecutor.executeScript("document.body.style.zoom='" + zoomFactor + "'");
+    }
+
+    @Test
+
+    public void UserInterface_ZoominOut_Test() throws InterruptedException {
+
+        Thread.sleep(3000);
+
+        zoomPage(driver,3.0);
+
+        Thread.sleep(3000);
+
+        zoomPage(driver,1.0);
+
+    }
+
+    @Test
+    public void Start_Button_Test() throws InterruptedException {
+
+
+        Thread.sleep(3000);
+
+        WebElement startButton = driver.findElement(By.xpath("/html/body/div/div/div/div[2]/div[1]/p"));
+
+        if (startButton.isDisplayed()) {
+            System.out.println(" START Butonu görüntülendi ");
+        }
+
+        if (startButton.isEnabled()) {
+            System.out.println(" START Butonu tıklama işlevine açık ");
+        }
+
+        startButton.click();
+
+        Thread.sleep(3000);
+
+        System.out.println(" Zamanlayıcı başarılı şekilde tetiklendi ");
+
+        if (!startButton.isEnabled()) {
+            System.out.println(" START butonunun geri sayım çalışırken tıklanabilir olmadığı görüldü   ");
+        } else {
+            System.out.println(" START Butonu geri sayım çalışırken tıklanabilir durumda fakat beklendiği üzere inaktif");
+        }
+
+    }
+
+    @Test
+    public void Stop_Button_Test() throws InterruptedException {
+        WebElement stopButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[2]/p"));
+
+        if (stopButton.isDisplayed()) {
+            System.out.println(" STOP Butonu görüntülendi ");
+        }
+
+        if (stopButton.isEnabled()) {
+            System.out.println(" STOP Butonu tıklama işlevine açık ");
+        }
+        Thread.sleep(3000);
+
+
+        stopButton.click();
+
+        Thread.sleep(3000);
+
+        if (stopButton.isEnabled()) {
+            System.out.println(" STOP Butonu gerisayım durduktan sonra tıklanabilir durumda fakat beklendiği üzere inaktif ");
+        }
+    }
+
+    @Test
+
+    public void Reset_Button_Test() throws InterruptedException {
+
+        WebElement resetButton = driver.findElement(By.xpath("//*[@id=\"root\"]/div/div/div[2]/div[3]/p"));
+
+
+        if (resetButton.isDisplayed()) {
+            System.out.println(" RESET Butonu görüntülendi ");
+        }
+
+        if (resetButton.isEnabled()) {
+            System.out.println(" RESET Butonu tıklama işlevine açık ");
+        }
+
+        Thread.sleep(3000);
+
+        resetButton.click();
+
+        if (resetButton.isEnabled()) {
+            System.out.println(" RESET Butonu , geri sayım resetlendikten sonra tıklanabilir durumda fakat beklendiği üzere inaktif ");
+        }
+
+    }
+
+ /*   @AfterClass
+
+    public static void tearDown(){
+        driver.close();
+    }
+*/
+
+}
